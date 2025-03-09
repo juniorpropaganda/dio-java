acc0c186e718759b9692ab023b262af2bc2a5247
juniorpropaganda <juniorpropaganda@gmail.com>
2025-03-09 16:26:14 -0300

    subir arquivos

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        //vamos ver se essa porra vai agora
+        //tentando fazer um commit
+    }
+}
